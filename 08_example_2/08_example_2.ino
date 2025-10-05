09da0fa5b6edad57dd00f7aa314eacad6bea9ecb
bell828 <bathsoap0820@gmail.com>
2025-10-05 22:41:47 +0900

    Add files via upload

---
+++ 08_example_2/08_example_2.ino
@@ -0,0 +1,60 @@
+#define PIN_LED  9
+#define PIN_TRIG 12
+#define PIN_ECHO 13
+
+#define SND_VEL 346.0
+#define INTERVAL 25
+#define PULSE_DURATION 10
+#define _DIST_MIN 100.0
+#define _DIST_MAX 300.0
+
+#define TIMEOUT ((INTERVAL / 2) * 1000.0)
+#define SCALE (0.001 * 0.5 * SND_VEL)
+
+unsigned long last_sampling_time;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TRIG, OUTPUT);
+  pinMode(PIN_ECHO, INPUT);
+  digitalWrite(PIN_TRIG, LOW);
+  Serial.begin(57600);
+}
+
+void loop() {
+  float distance;
+
+  if (millis() < (last_sampling_time + INTERVAL))
+    return;
+
+  distance = USS_measure(PIN_TRIG, PIN_ECHO);
+
+  int duty = 255;
+
+  if (distance >= 100 && distance <= 200) {
+    duty = map(distance, 100, 200, 255, 0);
+  } 
+  else if (distance > 200 && distance <= 300) {
+    duty = map(distance, 200, 300, 0, 255);
+  } 
+  else {
+    duty = 255;
+  }
+
+  analogWrite(PIN_LED, duty);
+
+  Serial.print("distance: ");
+  Serial.print(distance);
+  Serial.print(" mm, duty: ");
+  Serial.println(duty);
+
+  last_sampling_time += INTERVAL;
+}
+
+float USS_measure(int TRIG, int ECHO)
+{
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(PULSE_DURATION);
+  digitalWrite(TRIG, LOW);
+  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE;
+}
